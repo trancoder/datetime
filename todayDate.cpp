6ba0125bb6af37208bb67491d826fde7635019e4
tran <tran@localhost.localdomain>
2024-03-20 23:45:37 -0500

    update toddayDate.cpp

--- todayDate.cpp
+++ todayDate.cpp
@@ -17,8 +17,17 @@ int main() {
     // Convert time_t object to a tm struct for local time
     std::tm* local_time = std::localtime(&time_t_value);
 
+    // Extract hour, minute, and second from local time
+    int hour = local_time->tm_hour;
+    int minute = local_time->tm_min;
+    int second = local_time->tm_sec;
+
+    // Calculate seconds since midnight
+    int seconds_since_midnight = hour * 3600 + minute * 60 + second;
+
     // Print the current date and time
     std::cout << "Current date and time: " << std::asctime(local_time);
+    std::cout << "Seconds since midnight: " << seconds_since_midnight << std::endl;
 
     return 0;
 }
